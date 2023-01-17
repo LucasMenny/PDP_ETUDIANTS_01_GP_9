2254ef1775a7191eacfba9c863b9d89a0bf8859e
LucasMenny <lucas.menny@edu.ece.fr>
2023-01-17 12:05:19 +0100

    dfghjk

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,7 +1,9 @@
+#include <Arduino.h>
 #include <DHT.h> //inclur la library DTH pour le capteur de température DTH22
+#include <esp_sleep.h>
 
 #define pinDHT 33   // const pour savoir sur quel pin est brancher le capteur
-#define typeDeDHT DHT11          // Définir le type de DTH dans notre cas ------ DTH22
+#define typeDeDHT DHT11          // Définir le type de DTH dans notre cas ------ DHT11
 
 DHT dht(pinDHT, typeDeDHT); // déclaration de la fonction
 
@@ -10,32 +12,33 @@ DHT dht(pinDHT, typeDeDHT); // déclaration de la fonction
 void setup() 
 {
 
-  Serial.begin(115200); 
+  Serial.begin(9600); 
+
+   // configure deep sleep
+  esp_sleep_enable_timer_wakeup(5000); // wake up after 5 seconds
+  
   Serial.println("Programme de test du DHT22/DHT11");
   Serial.println("==========================");
   Serial.println();
+  
 
   
   dht.begin(); // Initialisation du DHT22
+  delay(500);
+  
+  Serial.print("Humidité = ");
+  Serial.print(dht.readHumidity());
+  Serial.println(" %");
+  
+  Serial.print("Température = ");
+  Serial.print(dht.readTemperature());
+  Serial.println(" °C");
+
+   // go to sleep
+  esp_deep_sleep_start();
 
 }
 
 void loop() 
 {
-
-
-  float humidite = dht.readHumidity();         // Stock dans la variable humidite le taux d'humidité (en %)
-  float temperature = dht.readTemperature();   // Stock dans la variable temperature, la température (en °C)
-
-  Serial.print("Humidité = ");
-  Serial.print(humidite);
-  Serial.println(" %");
-  
-  Serial.print("Température = ");
-  Serial.print(temperature);
-  Serial.println(" °C");
-  
-  delay(5000);
-
-
 }
