e960ad9c35fe9ba24afff9b4a4f2d74841227b00
LucasMenny <lucas.menny@edu.ece.fr>
2023-01-17 11:35:53 +0100

    dhxtfgyj

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,9 +1,41 @@
-#include <Arduino.h>
+#include <DHT.h> //inclur la library DTH pour le capteur de température DTH22
 
-void setup() {
-  // put your setup code here, to run once:
+#define pinDHT 33   // const pour savoir sur quel pin est brancher le capteur
+#define typeDeDHT DHT11          // Définir le type de DTH dans notre cas ------ DTH22
+
+DHT dht(pinDHT, typeDeDHT); // déclaration de la fonction
+
+
+
+void setup() 
+{
+
+  Serial.begin(115200); 
+  Serial.println("Programme de test du DHT22/DHT11");
+  Serial.println("==========================");
+  Serial.println();
+
+  
+  dht.begin(); // Initialisation du DHT22
+
 }
 
-void loop() {
-  // put your code here to run repeatedly
+void loop() 
+{
+
+
+  float humidite = dht.readHumidity();         // Stock dans la variable humidite le taux d'humidité (en %)
+  float temperature = dht.readTemperature();   // Stock dans la variable temperature, la température (en °C)
+
+  Serial.print("Humidité = ");
+  Serial.print(humidite);
+  Serial.println(" %");
+  
+  Serial.print("Température = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+  
+  delay(5000);
+
+
 }
